a824737fa5fdc096a8c81b252ff0bf6d33be8d26
Yoan01 <yoan.ranchon@gmail.com>
2022-02-14 13:58:57 +0100

    update index

--- Projet_Commerces_Alimentaires/index.php
+++ Projet_Commerces_Alimentaires/index.php
@@ -3,6 +3,18 @@
 <html>
 
     <head>
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+        <meta charset="utf-8">
+        <title> Commerces Alimentaires </title>
+
+        <?php
+            include('bd.php');
+        ?>
+
+        <?php
+            $bdd = getBD();        
+        ?>
+
     
     </head>
 

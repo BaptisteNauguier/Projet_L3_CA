988f0982e7ba4f9c8dc80cdc7ac545acb14e6a9e
Raphael Nestor <nestor.martin.raphael@gmail.com>
2022-02-14 20:30:47 +0100

    retrait premier commit

--- page_inscription.php
+++ page_inscription.php
@@ -4,6 +4,12 @@
 		<title> page d'inscription </title>
 	</head>
 	<body>
-
+		<p> Nom : </p>
+		<p> Mot de passe </p>
+		<p> Prenom : </p>
+		<p> Confirmation de mot de passe </p>
+		<p> Nom d'utilisateur: </p>
+		<p> Valider </p>
+		<p> Adresse email : </p>
 	</body>
 </html>

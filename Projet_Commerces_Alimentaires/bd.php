1fd1f1d71acec212579fa38e9ac4df0702578f1c
Yoan01 <yoan.ranchon@gmail.com>
2022-02-15 16:38:49 +0100

    update bd.php

--- Projet_Commerces_Alimentaires/bd.php
+++ Projet_Commerces_Alimentaires/bd.php
@@ -1,6 +1,6 @@
 <?php
     function getBD(){
-    $bdd = new PDO('mysql:host=localhost;dbname=SportPlus;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=projet_ca;charset=utf8', 'root', 'root');
     return $bdd;
     }
 ?>

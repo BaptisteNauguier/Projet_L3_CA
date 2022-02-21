ad1f144591027c88fd71d950b4b06f8f0dc0ce57
BaptisteNauguier <bnauguier@gmail.com>
2022-02-21 15:10:35 +0100

    ajout page filtre et css pour la page filtre ainsi que la page activite.php

---
+++ Projet_Commerces_Alimentaires/recherche_restaurant/Activite.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+   <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <link rel="stylesheet" href="../../Projet_Commerces_Alimentaires/css/CssCA.css" type="text/css"/> 
+      <title>Boucherie</title>
+      <?php
+            require('../bd.php');  #importation de la base de donnee
+            $bdd = getBD();     
+        ?> 
+   </head>
+   <body>
+       <ul>
+   <?php
+        $activite = $_GET["activite"]; 
+        if($_GET["activite"] == "autre"){
+            $rep = $bdd->query("SELECT DISTINCT commerce_alimentaire.nom_etablissement, commerce_alimentaire.niveau_sanitaire , commerce_alimentaire.Adresse,commerce_alimentaire.id_commerce
+        from commerce_alimentaire, activite
+        where commerce_alimentaire.id_activite not in (SELECT activite.id_activite from activite where activite.Activite_etablissement LIKE '%boucherie%' 
+        or activite.Activite_etablissement LIKE '%poissonnerie%' or activite.Activite_etablissement LIKE '%boulangerie%' or activite.Activite_etablissement LIKE '%patisserie%'
+        or activite.Activite_etablissement LIKE '%charcuterie%' or activite.Activite_etablissement LIKE '%chocolatier%' or activite.Activite_etablissement LIKE '%fromagerie%'
+        or activite.Activite_etablissement LIKE '%glacier%' or activite.Activite_etablissement LIKE '%restaurant%' or activite.Activite_etablissement LIKE '%traiteur%')
+        ORDER BY commerce_alimentaire.id_commerce");
+         }
+         else {
+            $rep = $bdd->query("SELECT DISTINCT commerce_alimentaire.nom_etablissement, commerce_alimentaire.niveau_sanitaire , commerce_alimentaire.Adresse,commerce_alimentaire.id_commerce
+            from commerce_alimentaire, activite
+            where commerce_alimentaire.id_activite in (SELECT activite.id_activite from activite where activite.Activite_etablissement LIKE '%".$activite."%')
+            ORDER BY commerce_alimentaire.id_commerce");
+
+         }
+
+        while ($mat=$rep->fetch()) { #PDO::FETCH_ASSOC dans le fetch
+
+            echo "<li>".$mat["nom_etablissement"]."</li></br>"; # on peut rajouter ."</br>"
+        }
+
+        $rep ->closeCursor();
+        ?>
+</ul>
+
+
+
+
+
+   </body>
+</html>

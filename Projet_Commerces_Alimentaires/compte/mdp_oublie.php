b7cf5eacf4e5ef88fd0a951a0f74c6e58c719f1f
Poupelin Chloé <chloepoupelin@hotmail.fr>
2022-03-04 11:02:08 +0100

    changement mdp_oublie

--- Projet_Commerces_Alimentaires/compte/mdp_oublie.php
+++ Projet_Commerces_Alimentaires/compte/mdp_oublie.php
@@ -3,9 +3,9 @@
 <html>
 
     <head>
-        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/style_mdp_oublie.css" type="text/css" media="screen" />
         <meta charset="utf-8">
-        <title> Commerces Alimentaires </title>
+        <title> Mot de passe oublié </title>
 
         <?php
             include('bd.php');
@@ -23,13 +23,13 @@
 	<h1> Vous avez oublié votre mot de passe ? </h1>
 	
 	<form action="index.php" method="get" autocomplete="off">
-<p>
+<p class="mail">
 Adresse mail :
 <input type="text" name="mail" value=""/>
 </p>
    
 	
-	<p> Nous allons envoyer un mail avec un lien pour réinitialiser votre mot de passe </p> <! phrase écrite en dessous en tout petit et couleur gris pâle>
+	<p class="phrase"> Nous allons envoyer un mail avec un lien pour réinitialiser votre mot de passe </p> <! phrase écrite en dessous en tout petit et couleur gris pâle>
 	
 <p>
 <input type="submit" value="Envoyer">

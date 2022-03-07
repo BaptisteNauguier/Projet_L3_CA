e8031749fc144f9149876217707a55297532a42b
Raphael Nestor <nestor.martin.raphael@gmail.com>
2022-03-07 03:03:25 +0100

    page commerce

--- Projet_Commerces_Alimentaires/page_commerce.php
+++ Projet_Commerces_Alimentaires/page_commerce.php
@@ -10,18 +10,21 @@
 		?>
 	</head>
 	<body>
+	<!-- entete -->
 		<?php $titre = $bdd -> query('select 'nom_etablissement' from 'commerce_alimentaire' where 'Id_commerce' = id_page') 
 			echo '<p id = 'titre'> $titre </p>'
 		?>
 		</br>
 		<p> Site internet : </p>
 		<p> <a href="lien commerce"> lien du site internet </a></p>
-		
+	
+	<!-- localisation -->
 		<div class = "col1">
 			<p> Localisation : </p>
 			<p> <a href="lien localisation"> localisation </a></p>
 		</div>
 		
+	<!-- Avis clients -->
 		<div class = "col2">
 			<p> Avis Clients : </p>
 			<?php
@@ -29,13 +32,18 @@
 				echo '<p> $pseudo </p>'
 				$avis = $bdd -> query('select forum_ca.avis from forum_ca, utilisateur_inscrit where forum_ca.id_UI = utilisateur_inscrit.id_UI')
 				echo '<p> $avis </p>'
+				$pseudo = $bdd -> query('select pseudo from utilisateur_inscrit where ')
+				echo '<p> $pseudo </p>'
+				$avis = $bdd -> query('select forum_ca.avis from forum_ca, utilisateur_inscrit where forum_ca.id_UI = utilisateur_inscrit.id_UI')
+				echo '<p> $avis </p>'
+				$pseudo = $bdd -> query('select pseudo from utilisateur_inscrit where ')
+				echo '<p> $pseudo </p>'
+				$avis = $bdd -> query('select forum_ca.avis from forum_ca, utilisateur_inscrit where forum_ca.id_UI = utilisateur_inscrit.id_UI')
+				echo '<p> $avis </p>'
 			?>
-			
-			<img id = "img" src="photo de profil client" alt="Profil client">
-			<img id = "img" src="image étoiles" alt="Etoiles">
-			<p> avis du client </p>
 		</div>
 		
+	<!-- Etat sanitaire -->
 		<div class = "col3">
 			<p> Etat sanitaire : </p>
 			<?php

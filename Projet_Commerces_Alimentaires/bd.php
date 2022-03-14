4c681c5c8868ae930fc1136e979fb76d333f5680
BaptisteNauguier <bnauguier@gmail.com>
2022-03-14 15:23:56 +0100

    modification mot de passe en 'root' 'root'

--- Projet_Commerces_Alimentaires/bd.php
+++ Projet_Commerces_Alimentaires/bd.php
@@ -1,6 +1,6 @@
 <?php
     function getBD(){
-    $bdd = new PDO('mysql:host=localhost;dbname=projet_ca;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=projet_ca;charset=utf8', 'root', 'root');
     return $bdd;
     }
 ?>

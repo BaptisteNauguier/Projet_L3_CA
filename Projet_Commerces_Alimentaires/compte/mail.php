032ae0ccc2e52b0614daed101ee0db04bdae9792
Yoan01 <yoan.ranchon@gmail.com>
2022-03-14 14:09:55 +0100

    ajout mail.php

---
+++ Projet_Commerces_Alimentaires/compte/mail.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title> Envoie mail </title>
+        <meta charset="utf-8">
+
+        <?php
+        
+            #echo '<meta http-equiv="refresh" content=0;URL="../index.php">';
+            
+
+        ?>
+
+    </head>
+
+    <body>
+
+            <?php 
+
+                mail("yoyo.1601.1601@gmail.com", "test d'envoie", "cest un test");
+
+
+            ?>
+    
+    </body>
+
+</html>

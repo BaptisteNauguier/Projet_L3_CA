6766e340da12a069fd9b5a6368e3f080ce42b7f6
Yoan01 <yoan.ranchon@gmail.com>
2022-03-15 09:53:00 +0100

    modif inscription.php

--- Projet_Commerces_Alimentaires/compte/inscription.php
+++ Projet_Commerces_Alimentaires/compte/inscription.php
@@ -3,59 +3,51 @@
 <html>
 
     <head>
+
         <link rel="stylesheet" href="css/style_inscription.css" type="text/css" media="screen" />
         <meta charset="utf-8">
         <title> Inscription </title>
-
-        <?php
-            include('bd.php');
-        ?>
-
-        <?php
-            $bdd = getBD();        
-        ?>
-
     
     </head>
 
     <body>
 
-	
-	<form action="index.php" method="get" autocomplete="off">
-<p>
-Nom :
-<input type="text" name="n" value=""/>
-</p>
-    
-    <p>
-Prénom :
-<input type="text" name="p" value=""/>
-</p>
+                
+        <form action="enregistrement.php" method="post" autocomplete="off">
+            <p>
+            Nom :
+            <input type="text" name="n" value="<? echo $_GET['n'] ?>"/>
+            </p>
+                
+            <p>
+            Prénom :
+            <input type="text" name="p" value="<? echo $_GET['p'] ?>"/>
+            </p>
 
- <p>
-Nom d'utilisateur:
-<input type="text" name="nd" value=""/>
-</p>
-    
-     <p>
-Adresse email:
-<input type="text" name="mail" value=""/>
-</p>
-    
- <p>
-Mot de passe :
-<input type="password" name="mdp1" value=""/>
-</p>
+            <p>
+            Nom d'utilisateur:
+            <input type="text" name="pseudo" value="<? echo $_GET['pseudo'] ?>"/>
+            </p>
+                
+            <p>
+            Adresse email:
+            <input type="text" name="mail" value="<? echo $_GET['mail'] ?>"/>
+            </p>
+                
+            <p>
+            Mot de passe :
+            <input type="password" name="mdp1" value=""/>
+            </p>
 
-<p>
-Confirmation mot de passe :
-<input type="password" name="mdp2" value=""/>
-</p>  
+            <p>
+            Confirmation mot de passe :
+            <input type="password" name="mdp2" value=""/>
+            </p>  
 
-<p class="submit">
-<input type="submit" value="Valider">
-</p>
-</form>
+            <p class="submit">
+            <input type="submit" value="Valider">
+            </p>
+        </form>
 
 	
     </body>

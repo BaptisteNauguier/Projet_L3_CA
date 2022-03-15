8393c319ae3ff4d09e9be573a399e35fc20bc895
Leonardo Irala <leonardoirapa@hotmail.com>
2022-03-15 14:34:29 +0100

    index du forum

--- Projet_Commerces_Alimentaires/forum/index.php
+++ Projet_Commerces_Alimentaires/forum/index.php
@@ -4,6 +4,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link href="../css/style_forum.css" rel="stylesheet">
+	<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- simboles -->
 	<title>Forum</title>
 
 <?php 
@@ -12,7 +14,10 @@ $bdd = getBD();
 
 session_start();
 
-$rep = $bdd->query('select * from commentaire_site cs INNER JOIN donner do ON do.id_site = cs.id_site INNER JOIN utilisateurs_inscrit ui ON ui.id_ui = do.id_ui');
+
+//id_commerce = 0, c'est pour differencier les avis "site" et les avis "commerces"
+$rep = $bdd->query('select * from forum_ca ca INNER JOIN utilisateurs_inscrit ui ON ui.id_UI = ca.id_UI  WHERE ca.id_commerce = 0'); 
+
 $count = $rep ->rowCount(); //compter s'il y en a des resultats
 
 
@@ -20,6 +25,7 @@ $count = $rep ->rowCount(); //compter s'il y en a des resultats
 ?>
 </head>
 <body>
+
 	<!-- MENU -->
     <nav class="navbar">
         <ul class="navbar-ul">
@@ -35,41 +41,115 @@ $count = $rep ->rowCount(); //compter s'il y en a des resultats
 			<li class="nav-item">
                 <a class="nav-link" href="../historique/historique.php">Historique</a>
             </li>
-			<li class="nav-item">
-                <a class="nav-link" href="../connecte_toi.php">Connecte-toi</a>
+			
+			<!-- ------ MENU SI L'UTILISATEUR EST CONNECTE ------ -->
+			<?php if(isset($_SESSION['utilisateur'])) { ?>
+			<li class="nav-item dropdown">
+                <a class="nav-link dropbtn" href="#">Bienvenue <?php echo $_SESSION['utilisateur']['pseudo'] ?> ▼</a>
+                <div class="dropdown-content">
+                        <a href='../session/deconnexion.php'>Se deconnecter</a>
+                </div>
             </li>
+			<!-- ------ FIN MENU SI L'UTILISATEUR EST CONNECTE ------ -->
+			
+			<?php }else{ ?>
+			<li class="nav-item">
+                <a class="nav-link" href="../session/connexion.php">Connecte-toi</a>
+            </li>
+			<?php } ?>
         </ul>
     </nav>
 	<!-- FIN MENU -->
 	<main>
 		<h2 class="text-center titre">Forum</h2>
-		<h3>Votre avis nous intéresse !</h3>
-		<p><b>Une remarque sur l'application, un commerce en particulier? Echangez avec les utilisateur de EzFood</b></p>
-		<form action="commentaire-forum.php" method="get" autocomplete="off">
-			Commentaire:
-			<textarea id="commen" name="commen" rows="4" cols="50"></textarea><br>
-			<input type="submit" name="submit" value="Envoyer">
-		</form>
-		<?php if(isset($_SESSION['client'])) { ?>
+		<div class="avis">
+			<h3 class="col">Votre avis nous intéresse !</h3>
+			<?php if(isset($_SESSION['utilisateur'])) { ?> 
+				<div class="col">
+					<button id="open" class="ouvrir-fermer">
+						<i class="fa fa-plus" aria-hidden="true"></i>
+						Ajouter un commentaire
+					</button>
+					<button id="close" class="ouvrir-fermer">
+						<i class="fa fa-minus" aria-hidden="true"></i>
+						Ajouter un commentaire
+					</button> 
+				</div>
+			<?php } ?>
+		</div>
+		<b>Une remarque sur l'application? Echangez avec les utilisateur de EzFood</b>
+		
+		<!-- SI L'UTILISATEUR EST CONNECTE ON PEUT AJOUTER UN COMMENTAIRE -->
+		<?php if(isset($_SESSION['utilisateur'])) { ?>
+			<section class="frm-commentaire">
+				<form action="commentaire.php" method="get" autocomplete="off">
+					<div class="d-flex">
+						<span class="text-light">Commentaire</span>
+						<div class="col">
+							<input type="hidden" value="<?php echo $_SESSION['utilisateur']['id'] ?>"  name="id_ui" id="id_ui" >
+							<input type="hidden" value="add_commentaire"  name="status" id="status" >
+							<div class="commen-textarea">
+								<textarea id="commentaire" required name="commentaire" rows="4" cols="50"></textarea>
+							</div>
+							<div class="submit-input">
+								<input type="submit" name="submit" value="Ajouter le commentaire">
+							</div>
+						</div>
+					</div>		
+				</form>
+			</section>
 		<?php } ?>
+		<!-- ----------------------------------- -->
+		
+		<div class="d-flex">
             <?php 
 				if($count > 0){ //si on a un commentaire afficher
 					while ($ligne = $rep ->fetch()) {	
-					//<a href="https://www.flaticon.com/fr/icones-gratuites/defaut" title="défaut icônes">Défaut icônes créées par Ilham Fitrotul Hayat - Flaticon</a>?>					
-						<div class="d-flex">
-							<div class="p-photo"><img src="../img/utilisateur.png" width="20" height="20"></div>
-							<p class="nom-date"><?php echo $ligne['pseudo']." " ?><small class="text-light"><?php echo date('d/m/Y H:i:s', strtotime($ligne['date'])); ?></small></p>
+						?>					
+						<div class="col">
+							<div class="d-flex">
+								<div class="p-photo"><i class="fa fa-user-circle-o" aria-hidden="true"></i></div>
+								<p class="nom-date"><?php echo $ligne['pseudo']." " ?><small class="text-light"><?php echo date('d/m/Y H:i:s', strtotime($ligne['date'])); ?></small></p>
+							</div> 
+							<p><?php echo $ligne['avis'] ?></p>
+							<?php if(isset($_SESSION['utilisateur']) && $_SESSION['utilisateur']['id'] == $ligne["id_UI"]){ ?>	
+															
+									<div class="supp-comm">		
+										<form action="commentaire.php" method="get" autocomplete="off">										
+											<input type="hidden" value="<?php echo $_SESSION['utilisateur']['id'] ?>"  name="id_ui" id="id_ui" >
+											<input type="hidden" value="<?php echo $ligne["id_forum"] ?>"  name="id_forum" id="id_forum" >	
+											<button type="submit" class="remove"><i class="fa fa-trash"></i></button>
+											<input type="hidden" value="delete_commentaire" name="status" id="status" >
+										</form>
+									</div>								
+							<?php } ?>
 						</div> 
-						<p><?php echo $ligne['commentaire'] ?></p>
 			<?php
 					}
 				}else{ //si on a pas des commentaires
-					echo "Pas encore de commentaire! Soyez le premier.";
+					echo "<p>Pas encore de commentaire! <a class='s-inscrire' href='inscription.php'>Soyez le premier</a></p>";
                 }
                 $rep ->closeCursor();  
             ?>
+		</div>	
 			
-		
 	</main>
+	<script>
+	$(document).ready(function() {
+		$("#close").css({"display": "none"});
+		
+        $("#open").click(function(){
+			$(".frm-commentaire").css({"display": "block"});
+			$("#close").css({"display": "block"});
+			$("#open").css({"display": "none"});
+		});
+		
+		$("#close").click(function(){
+			$(".frm-commentaire").css({"display": "none"});
+			$("#open").css({"display": "block"});
+			$("#close").css({"display": "none"});
+		});
+	});
+	</script>
 </body>
 </html>

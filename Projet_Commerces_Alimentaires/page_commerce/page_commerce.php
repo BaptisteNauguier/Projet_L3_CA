b297a42c828848dee4e7d649327f824b89782a2d
Yoan01 <yoan.ranchon@gmail.com>
2022-03-21 14:10:40 +0100

    modif page_commerce.php

--- Projet_Commerces_Alimentaires/page_commerce/page_commerce.php
+++ Projet_Commerces_Alimentaires/page_commerce/page_commerce.php
@@ -1,75 +1,51 @@
 <!DOCTYPE html>
 <html lang = "fr">
 	<head>
-		<link rel="stylesheet" href="../../css/style_commerce.css" />
+		<link rel="stylesheet" href="../css/style_trouve.css" />
 		<title> page commerce </title>
 		<?php
-			# $id_page = get(id_commerce)
+			$id = $_GET['id_commerce'];
 			include('../bd.php');
 			$bdd = getBD();
 		?>
 	</head>
+
 	<body>
-		<?php/*
-			$titre = $bdd -> query('select 'nom_etablissement' from 'commerce_alimentaire' where 'Id_commerce' = id_page') 
-			echo '<p id = 'titre'> $titre </p>'
-		*/?>
-		</br>
-		<p> Site internet : </p>
-		<!-- <p> <a href="lien commerce"> lien du site internet </a></p>
-		
-		<div class = "col1">
-			<p> Localisation : </p>
-			<p> <a href="lien localisation"> localisation </a></p>
-		</div>
-		
-		<div class = "col2">
-			<p> Avis Clients : </p>
-			<?php/*
-				$pseudo = $bdd -> query('select pseudo from utilisateur_inscrit where ')
-				echo '<p> $pseudo </p>'
-				$avis = $bdd -> query('select forum_ca.avis from forum_ca, utilisateur_inscrit where forum_ca.id_UI = utilisateur_inscrit.id_UI')
-				echo '<p> $avis </p>'
-			*/?>
-			
-			<img id = "img" src="photo de profil client" alt="Profil client">
-			<img id = "img" src="image étoiles" alt="Etoiles">
-			<p> avis du client </p>
-			
-		</div>
-		
-		<div class = "col3">
-			<p> Etat sanitaire : </p>
-			<?php/*
-				if ("Très satisfaisant" = $bdd->query('select 'niveau_sanitaire' from 'commerce_alimentaire' where 'Id_commerce' = id_page')
-					echo "X  Très satisfaisant"
-					}
-				else {
-					echo "   Très satisfaisant"
-				}
-			
-				if ("Satisfaisant" = $bdd->query('select 'niveau_sanitaire' from 'commerce_alimentaire' where 'Id_commerce' = id_page')
-					echo "X  Satisfaisant"
-				}
-				else {
-					echo "   Satisfaisant"
-				}
-			
-				if ("A améliorer" = $bdd->query('select 'niveau_sanitaire' from 'commerce_alimentaire' where 'Id_commerce' = id_page')
-					echo "X  A améliorer"
-				}
-				else {
-					echo "   A améliorer"
-				}
-			
-				if ("A corriger de manière urgente" = $bdd->query('select 'niveau_sanitaire' from 'commerce_alimentaire' where 'Id_commerce' = id_page')
-					echo "X  A corriger de manière urgente"
-				}
-				else {
-					echo "   A corriger de manière urgente"
-				}
-			*/?>
-		</div>
-		-->
+		<?
+			$donnee = $bdd -> query('Select DISTINCT commerce_alimentaire.nom_etablissement, commerce_alimentaire.coordonnee, commerce_alimentaire.niveau_sanitaire, forum_ca.avis, lien_map.lien, utilisateurs_inscrit.pseudo
+			FROM forum_ca, commerce_alimentaire, lien_map, utilisateurs_inscrit
+			WHERE forum_ca.id_commerce=commerce_alimentaire.id_commerce
+			AND lien_map.id_commerce=commerce_alimentaire.id_commerce
+			AND utilisateurs_inscrit.id_UI=forum_ca.id_UI
+			AND commerce_alimentaire.id_commerce='.$id.''); /* importation des données */
+
+			$r_donnee = $donnee -> fetch();
+
+			echo 
+			'<h1>'.$r_donnee['nom_etablissement'].'</h1></br>
+			<div>
+			<p> Site internet : 
+			<a href="page_commerce.php?id_commerce='.$id.'"> lien site internet </a> </br>
+			</div>
+			<div class=col1>
+			Localisation : </br>
+			'.$r_donnee['lien'].'
+			</br>
+			</div>
+			<div class=col1>
+			Avis clients :'
+			.$r_donnee['pseudo'].' :'
+			.$r_donnee['avis'].'</br>
+			</div>
+			<div class=col1>
+			Etat sanitaire :'
+			.$r_donnee['niveau_sanitaire'].'</br> </p>
+			</div>
+			'
+			;
+
+			$donnee -> closeCursor();
+
+		?>
 	</body>
 </html>

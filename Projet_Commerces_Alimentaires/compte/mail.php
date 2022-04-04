6f20a9b0daf3d23e7bc147afcb5755ee4a1e0185
Yoan01 <yoan.ranchon@gmail.com>
2022-04-04 13:11:24 +0200

    modif mail

--- Projet_Commerces_Alimentaires/compte/mail.php
+++ Projet_Commerces_Alimentaires/compte/mail.php
@@ -8,8 +8,9 @@
         <meta charset="utf-8">
 
         <?php
-        
-            #echo '<meta http-equiv="refresh" content=0;URL="../index.php">';
+
+            email();
+            echo '<meta http-equiv="refresh" content=10;URL="../index.php">';
             
 
         ?>
@@ -19,10 +20,17 @@
     <body>
 
             <?php 
-
-                mail("yoyo.1601.1601@gmail.com", "test d'envoie", "cest un test");
-
-
+                function email(){
+                    ini_set( 'display_errors', 1 );
+                    error_reporting( E_ALL );
+                    $from = "commerces.alimentaires@hotmail.com";
+                    $to = "commerces.alimentaires@hotmail.com";
+                    $subject = "mdp oublie";
+                    $message = "Veuillez cliquer sur le lien pour changer votre mdp :";
+                    $header = "From :" . $from;
+                    mail($to,$subject,$message,$header);
+                }
+               
             ?>
     
     </body>

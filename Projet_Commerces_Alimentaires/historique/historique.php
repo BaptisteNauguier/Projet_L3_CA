af50de6dc5e5b797f6dfe86bb26b90d666ac5b25
Leonardo Irala <leonardoirapa@hotmail.com>
2022-04-05 15:58:08 +0200

    Partie historique

--- Projet_Commerces_Alimentaires/historique/historique.php
+++ Projet_Commerces_Alimentaires/historique/historique.php
@@ -19,59 +19,65 @@
     </head>
 
     <body>
-            <!-- MENU -->
-        <nav class="navbar row">
-                <div class="col-1">
-                    <ul class="navbar-ul">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../"><i class="fa fa-home"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2 text-center">
-                    <ul class="navbar-ul">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Trouve ton commerce</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../forum/index.php">Forum</a>
-                        </li>
-                        <?php if(isset($_SESSION['utilisateur'])) { ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../historique/historique.php">Historique</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../historique/historique.php">Favori</a>
-                        </li>
-                        <?php  } ?>
-                    </ul>
-                </div>
-                <div class="col-3">
-                    <ul class="navbar-ul menu-right">
-                    <?php if(isset($_SESSION['utilisateur'])) { ?>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropbtn" href="#">Bienvenue <?php echo $_SESSION['utilisateur']['pseudo'] ?> ▼</a>
-                            <div class="dropdown-content">
-                                    <a href='../session/deconnexion.php'>Se deconnecter</a>
-                            </div>
-                        </li>
-                        <?php }else{ ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../session/connexion.php">Se connecter</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../compte/inscription.php">S'inscrire</a>
-                        </li>
-                    <?php } ?>
-                    </ul>
-                </div>
-        </nav>
-        <!-- FIN MENU -->
+        <!-- MENU -->
+    <nav class="navbar row">
+			<div class="col-1">
+				<ul class="navbar-ul">
+					<li class="nav-item">
+						<a class="nav-link" href="../"><i class="fa fa-home"></i></a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-2 text-center">
+				<ul class="navbar-ul">
+					<li class="nav-item">
+						<a class="nav-link" href="../trouve/">Trouve ton commerce</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="../forum/">Forum</a>
+					</li>
+					<?php if(isset($_SESSION['utilisateur'])) { ?>
+					<li class="nav-item">
+						<a class="nav-link active" href="../historique/historique.php">Historique</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="../favori/">Favori</a>
+					</li>
+					<?php  } ?>
+				</ul>
+			</div>
+			<div class="col-3">
+				<ul class="navbar-ul menu-right">
+				<?php if(isset($_SESSION['utilisateur'])) { ?>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropbtn" href="#">Bienvenue <?php echo $_SESSION['utilisateur']['pseudo'] ?> ▼</a>
+						<div class="dropdown-content">
+								<a href='../session/profil.php'>Mon profil</a>
+								<a href='../session/deconnexion.php'>Se deconnecter</a>
+						</div>
+					</li>
+					<?php }else{ ?>
+					<li class="nav-item">
+						<a class="nav-link" href="../session/connexion.php">Se connecter</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="../compte/inscription.php">S'inscrire</a>
+					</li>
+				<?php } ?>
+				</ul>
+			</div>
+    </nav>
+	<!-- FIN MENU -->
         <section class="hist">
-        <h1>
-            Historique des commerces visités
-        </h1>
+		<h2 class="text-center titre">Historique des commerces visités</h2>	
+		<?php
+		
+		if(isset($_SESSION['utilisateur'])){
 
+            $hist = $bdd -> query('select commerce_alimentaire.*, historique.date FROM commerce_alimentaire, historique WHERE commerce_alimentaire.id_commerce=historique.id_commerce and id_UI='.$_SESSION['utilisateur']['id'].''); /*'.$_SESSION['client']['id'].'*/
+			$row = $hist->rowCount();
+			
+			if($row>0){ ?>
         <div class="div_hist">
         <table class="t_hist">    <!-- création tableau -->
             <tr>
@@ -83,10 +89,8 @@
                 <th class="t_hist">Date</th>
             </tr>
 
-            <?php
-
-            $hist = $bdd -> query('select commerce_alimentaire.*, historique.date FROM commerce_alimentaire, historique WHERE commerce_alimentaire.id_commerce=historique.id_commerce and id_UI=1'); /*'.$_SESSION['client']['id'].'*/
-
+            
+			<?php
             while ($r_hist = $hist -> fetch()) {
 
                 echo  /* affichage des données sous forme de tableau*/
@@ -107,7 +111,8 @@
 
         </table>
         </div>
-    
+		<?php }else echo "Vous n'avez pas encore visité un commerce"; 
+		}else echo "Vous devez <a href='../session/connexion.php'>vous connecter</a> pour acceder a votre historique."?>
         </section>
     </body>
 

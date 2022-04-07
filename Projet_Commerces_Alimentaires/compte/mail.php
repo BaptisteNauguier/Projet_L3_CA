fd21cf868cb85fa6012b8b856a92f6f4f3be6456
Yoan01 <yoan.ranchon@gmail.com>
2022-04-07 17:00:45 +0200

    changement mail.php

--- Projet_Commerces_Alimentaires/compte/mail.php
+++ Projet_Commerces_Alimentaires/compte/mail.php
@@ -9,9 +9,15 @@
 
         <?php
 
-            email();
-            echo '<meta http-equiv="refresh" content=10;URL="../index.php">';
-            
+            include('../bd.php');
+
+            if (verif($_GET['mail'])=="") {
+                echo '<meta http-equiv="refresh" content=0;URL="mdp_oublie.php?q=mauvais+mail">';
+            }
+            else {
+                email($_GET['mail']);
+                echo '<meta http-equiv="refresh" content=0;URL="../index.php">';
+            }
 
         ?>
 
@@ -20,15 +26,34 @@
     <body>
 
             <?php 
-                function email(){
-                    ini_set( 'display_errors', 1 );
-                    error_reporting( E_ALL );
+
+                function verif($mail) { /* vérification de l'existance du mail */
+
+                    $bdd = getBD();
+
+
+                    $check = $bdd->query('Select id_UI FROM utilisateurs_inscrit WHERE utilisateurs_inscrit.mail="'.$mail.'"');
+                    $verif=$check->fetch();
+                    return($verif['id_UI']);
+                
+                    $check->closeCursor();
+                }
+
+                function email($mail){  /* permet l'envoie du mail */
+
+                    $bdd = getBD(); 
+
+                    $id = $bdd -> query('select id_UI from utilisateurs_inscrit where utilisateurs_inscrit.mail="'.$mail.'"');
+                    $r = $id -> fetch();
+                
                     $from = "commerces.alimentaires@hotmail.com";
-                    $to = "commerces.alimentaires@hotmail.com";
-                    $subject = "mdp oublie";
-                    $message = "Veuillez cliquer sur le lien pour changer votre mdp :";
+                    $to = $mail;
+                    $subject = "Changement mot de passe";
+                    $message = "Veuillez cliquer sur le lien pour changer votre mdp : http://localhost:666/projet_commerces_alimentaires/compte/changement_mdp.php?id=".$r['id_UI']."";  /* le port est à changer suivant celui utilisé */
                     $header = "From :" . $from;
                     mail($to,$subject,$message,$header);
+
+                    $id -> closeCursor();
                 }
                
             ?>

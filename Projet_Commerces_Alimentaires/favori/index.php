be5474051f9b2475ab1665ac94c1d860a6bf27d7
Leonardo Irala <leonardoirapa@hotmail.com>
2022-04-08 21:00:05 +0200

    favori

--- Projet_Commerces_Alimentaires/favori/index.php
+++ Projet_Commerces_Alimentaires/favori/index.php
@@ -6,7 +6,7 @@
 	<link href="../css/style.css" rel="stylesheet">
 	<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- simboles -->
-	<title>Forum</title>
+	<title>Mes favoris</title>
 
 <?php 
 include('../bd.php');
